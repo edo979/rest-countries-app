eb0c2485619d6df3911530a35a9313f0aee9681d
edo979 <prezentabilan@gmail.com>
2022-09-10 08:41:41 +0200

    add components card and countries

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,21 @@ function App() {
 
         <section>
           <ul className="countries-list | grid-container">
-            <li className="card">belgia</li>
+            <li className="card">
+              <div className="card_img"></div>
+              <div className="card_body">
+                <h2>Germany</h2>
+                <p>
+                  Population: <span>81,770.900</span>
+                </p>
+                <p>
+                  Region: <span>Europe</span>
+                </p>
+                <p>
+                  Capital: <span>Berlin</span>
+                </p>
+              </div>
+            </li>
           </ul>
         </section>
       </main>
