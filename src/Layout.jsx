995a9907e344f9f2ee0b3d3819296f17dbf71311
edo979 <prezentabilan@gmail.com>
2022-09-11 18:43:14 +0200

    remember state and use it after click on back button

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,6 +1,93 @@
+import { useState, useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
 
 function Layout() {
+  const [state, setState] = useState({
+    countries: [],
+    selectedRegion: undefined,
+    inputValue: undefined,
+  })
+
+  useEffect(() => {
+    fetchData()
+
+    async function fetchData() {
+      try {
+        const response = await fetch('./data.json')
+
+        if (!response.ok) {
+          throw new Error('Network response was not OK')
+        }
+
+        const data = await response.json()
+
+        setState((prev) => ({ ...prev, countries: data }))
+      } catch (error) {
+        console.error(
+          'There has been a problem with your fetch operation:',
+          error
+        )
+      }
+    }
+  }, [])
+
+  const onInputChange = (e) => {
+    const inputValue = e.target.value
+
+    if (inputValue === '') {
+      // Reset filters
+      setState((prev) => ({
+        ...prev,
+        selectedRegion: undefined,
+        inputValue: undefined,
+      }))
+
+      return
+    }
+
+    setState((prev) => ({
+      ...prev,
+      inputValue,
+    }))
+  }
+
+  const onSelectChange = (selected) => {
+    const selectedRegion = selected.value
+
+    setState((prev) => ({
+      ...prev,
+      selectedRegion,
+    }))
+  }
+
+  const getCountries = () => {
+    // Filter functions
+    const filterByName = (country) =>
+      country.name.common.toLowerCase().startsWith(state.inputValue)
+
+    const filterByRegion = (country) =>
+      country.region.toLowerCase() === state.selectedRegion
+
+    let filteredCountries = []
+
+    if (state.inputValue && state.selectedRegion) {
+      filteredCountries = state.countries.filter(
+        (country) => filterByName(country) && filterByRegion(country)
+      )
+    } else if (state.inputValue) {
+      filteredCountries = state.countries.filter((country) =>
+        filterByName(country)
+      )
+    } else if (state.selectedRegion) {
+      filteredCountries = state.countries.filter((country) =>
+        filterByRegion(country)
+      )
+    } else {
+      return state.countries
+    }
+
+    return filteredCountries
+  }
   return (
     <>
       <header className="header | container flex">
@@ -13,7 +100,15 @@ function Layout() {
         </button>
       </header>
 
-      <Outlet />
+      <Outlet
+        context={{
+          onInputChange,
+          onSelectChange,
+          getCountries,
+          selectedRegion: state.selectedRegion,
+          inputValue: state.inputValue,
+        }}
+      />
     </>
   )
 }
--- src/components/CountriesList.jsx
+++ src/components/CountriesList.jsx
@@ -1,95 +1,16 @@
-import { useState, useEffect } from 'react'
+import { useOutletContext } from 'react-router-dom'
 
 import CountriesItem from './CountriesItem'
 import RegionSelect from './RegionSelect'
 
 function CountriesList() {
-  const [state, setState] = useState({
-    countries: [],
-    selectedRegion: undefined,
-    inputValue: undefined,
-  })
-
-  useEffect(() => {
-    fetchData()
-
-    async function fetchData() {
-      try {
-        const response = await fetch('./data.json')
-
-        if (!response.ok) {
-          throw new Error('Network response was not OK')
-        }
-
-        const data = await response.json()
-
-        setState((prev) => ({ ...prev, countries: data }))
-      } catch (error) {
-        console.error(
-          'There has been a problem with your fetch operation:',
-          error
-        )
-      }
-    }
-  }, [])
-
-  const onInputChange = (e) => {
-    const inputValue = e.target.value
-
-    if (inputValue === '') {
-      // Reset select by region filter
-      setState((prev) => ({
-        ...prev,
-        selectedRegion: undefined,
-        inputValue: undefined,
-      }))
-
-      return
-    }
-
-    setState((prev) => ({
-      ...prev,
-      inputValue,
-    }))
-  }
-
-  const onSelectChange = (selected) => {
-    const selectedRegion = selected.value
-
-    setState((prev) => ({
-      ...prev,
-      selectedRegion,
-    }))
-  }
-
-  const getCountries = () => {
-    // Filter functions
-    const filterByName = (country) =>
-      country.name.common.toLowerCase().startsWith(state.inputValue)
-
-    const filterByRegion = (country) =>
-      country.region.toLowerCase() === state.selectedRegion
-
-    let filteredCountries = []
-
-    if (state.inputValue && state.selectedRegion) {
-      filteredCountries = state.countries.filter(
-        (country) => filterByName(country) && filterByRegion(country)
-      )
-    } else if (state.inputValue) {
-      filteredCountries = state.countries.filter((country) =>
-        filterByName(country)
-      )
-    } else if (state.selectedRegion) {
-      filteredCountries = state.countries.filter((country) =>
-        filterByRegion(country)
-      )
-    } else {
-      return state.countries
-    }
-
-    return filteredCountries
-  }
+  const {
+    getCountries,
+    onInputChange,
+    onSelectChange,
+    selectedRegion,
+    inputValue,
+  } = useOutletContext()
 
   return (
     <main className="container">
@@ -107,13 +28,11 @@ function CountriesList() {
             id="search"
             placeholder="Search for a country..."
             onChange={onInputChange}
+            value={inputValue || ''}
           />
         </div>
 
-        <RegionSelect
-          onChange={onSelectChange}
-          selected={state.selectedRegion}
-        />
+        <RegionSelect onChange={onSelectChange} selected={selectedRegion} />
       </form>
 
       <section className="countries | m-top">
