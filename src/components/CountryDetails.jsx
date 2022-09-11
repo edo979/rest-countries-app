cf34e4e0ad93202ed5a4e00bd7c7ae799190a32b
edo979 <prezentabilan@gmail.com>
2022-09-11 22:49:28 +0200

    details basic layout

--- src/components/CountryDetails.jsx
+++ src/components/CountryDetails.jsx
@@ -6,16 +6,32 @@ function CountryDetails() {
     country = location.state
 
   return (
-    <div>
+    <main className="country | container">
       <button className="btn btn-primary" onClick={() => navigate(-1)}>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
           <path d="M109.3 288L480 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-370.7 0 73.4-73.4c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-128 128c-12.5 12.5-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 288z" />
         </svg>
         Back
       </button>
-      <p>{country.name.common}</p>
-      <p>{country.population.toLocaleString()}</p>
-    </div>
+
+      <article className="country-details">
+        <header>
+          <aside className="country_img">
+            <img src={country.flags.png} alt="" />
+          </aside>
+
+          <h2>{country.name.common}</h2>
+        </header>
+
+        <main className="details">
+          <ul className="details_list">
+            <li className="details_item">
+              {country.population.toLocaleString()}
+            </li>
+          </ul>
+        </main>
+      </article>
+    </main>
   )
 }
 
