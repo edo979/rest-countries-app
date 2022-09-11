ee4683ace30336c94388d3fa7284688761a194c8
edo979 <prezentabilan@gmail.com>
2022-09-11 18:15:48 +0200

    add back button to country details page

--- src/components/CountryDetails.jsx
+++ src/components/CountryDetails.jsx
@@ -1,11 +1,15 @@
-import { useLocation } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 
 function CountryDetails() {
-  const location = useLocation()
-  const country = location.state
+  const navigate = useNavigate(),
+    location = useLocation(),
+    country = location.state
 
   return (
     <div>
+      <button className="btn btn-primary" onClick={() => navigate(-1)}>
+        BACK
+      </button>
       <p>{country.name.common}</p>
       <p>{country.population.toLocaleString()}</p>
     </div>
