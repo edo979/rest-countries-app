f317f73f642280e95bbfb8cae466abfca543ab80
edo979 <prezentabilan@gmail.com>
2022-09-14 00:10:59 +0200

    add deploy script

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/rest-countries-app/',
   plugins: [react()],
   css: {
     devSourcemap: true,
